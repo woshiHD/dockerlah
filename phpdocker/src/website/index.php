71b42c821cec875b07884a8b75d28f51462b12b9
Brandon <brandonlau1406@gmail.com>
2021-08-18 17:28:18 +0800

    php docker experiment

---
+++ phpdocker/src/website/index.php
@@ -0,0 +1,21 @@
+<html>
+  <<head>
+    <<title>My Shop</title>
+  </head>
+
+  <<body>
+    <h1>Welcome</h1>
+    <ul>
+      <?php
+        $json = file_get_contents('http://product-service');
+        $obj = json_decode($json);
+
+        $products = $obj->products;
+        foreach ($products as $product) {
+          // code...
+          echo "<li>$product</li>";
+        }
+      ?>
+    </ul>
+  </body>
+</html>
